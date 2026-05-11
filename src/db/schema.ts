c5e976c7ba6abd86adb7d808aec56cdbe0f53d42
Abdul Halim <ahr.web.pro@gmail.com>
2026-05-11 16:48:09 +0600

    feat: Initialize NestJS project with essential configurations and dependencies.
    - added environment variables, ESLint and Prettier configurations.
    - basic application structure including database schema and example tests.

---
+++ src/db/schema.ts
@@ -0,0 +1,49 @@
+import {
+  boolean,
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from 'drizzle-orm/pg-core';
+
+export const userRoleEnum = pgEnum('user_role', ['user', 'admin']);
+
+export const users = pgTable('users', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  email: text('email').notNull().unique(),
+  passwordHash: text('password_hash').notNull(),
+  name: text('name').notNull(),
+  role: userRoleEnum('role').notNull().default('user'),
+  isVerified: boolean('is_verified').notNull().default(false),
+  verificationToken: text('verification_token'),
+  verificationTokenExpiresAt: text('verification_token_expires_at'),
+  resetToken: text('reset_token'),
+  resetTokenExpiresAt: text('reset_token_expires_at'),
+  refreshTokenHash: text('refresh_token_hash'),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});
+
+export const taskStatusEnum = pgEnum('task_status', [
+  'todo',
+  'in_progress',
+  'done',
+]);
+
+export const tasks = pgTable('tasks', {
+  id: uuid().defaultRandom().primaryKey(),
+  title: text('title').notNull(),
+  description: text('description'),
+  status: taskStatusEnum('status').notNull().default('todo'),
+  userId: uuid('user_id')
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});
+
+export type User = typeof users.$inferSelect;
+export type newUser = typeof users.$inferInsert;
+export type Task = typeof tasks.$inferSelect;
+export type newTask = typeof tasks.$inferInsert;
